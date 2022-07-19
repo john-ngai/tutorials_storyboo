d05f5411c1b818dc8eeb9e3c47980be7acd0c27f
mrjohnming <john.ngai@hotmail.com>
2022-07-19 10:37:07 -0400

    Add stories from ../src/components

--- 01_intro/taskbox/.storybook/main.js
+++ 01_intro/taskbox/.storybook/main.js
@@ -1,5 +1,9 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  stories: ['../src/components/**/*.stories.js'],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
