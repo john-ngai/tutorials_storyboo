9f981513bb7ecf0212a1965ba44f05339eee4b7e
mrjohnming <john.ngai@hotmail.com>
2022-07-19 10:10:35 -0400

    Create a Task component

---
+++ 01_intro/taskbox/src/components/Tasks.js
@@ -0,0 +1,9 @@
+export default function({ task: { id, title, state }, onArchiveTask, onPinTask }) {
+  return (
+    <div className="list-item">
+      <label htmlFor="title" aria-label={title}>
+        <input type="text" value={title} readOnly={true} name="title" />
+      </label>
+    </div>
+  );
+}
